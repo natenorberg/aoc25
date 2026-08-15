57308d40a986cecba81a3acb9c08aca75dd01daa
Nate Norberg <nate.norberg@quiq.com>
2026-08-14 23:45:47 -0600

    Day 08 / Part 1

--- src/day08.rs
+++ src/day08.rs
@@ -1,9 +1,14 @@
 use std::fs;
 
-pub fn part1(filename: &str, num_connections: u32) -> u64 {
+pub fn part1(filename: &str, num_connections: usize) -> u64 {
     let input = fs::read_to_string(filename).expect("Couln't read the file");
     let boxes = parse_input(&input);
-    0
+    let connections = build_connections(&boxes);
+    let mut circuits = init_circuits(boxes.len());
+
+    join_closest_circuits(num_connections, &mut circuits, &connections);
+    circuits.sort_by(|a, b| b.box_ids.len().cmp(&a.box_ids.len()));
+    get_score(&circuits)
 }
 
 pub fn part2(filename: &str) -> u64 {
@@ -21,12 +26,49 @@ struct Box {
 
 #[derive(Debug)]
 struct Connection {
-    a: Box,
-    b: Box,
+    from_idx: usize,
+    to_idx: usize,
     distance: f64,
 }
 
+#[derive(PartialEq, Clone)]
+struct Circuit {
+    box_ids: Vec<usize>,
+}
+
+impl Circuit {
+    fn merge(&mut self, other: &Circuit) {
+        self.box_ids.extend_from_slice(&other.box_ids);
+    }
+
+    fn has_box(&self, box_id: usize) -> bool {
+        self.box_ids.contains(&box_id)
+    }
+}
+
 // Logic ======================================================================
+
+fn join_closest_circuits(n: usize, circuits: &mut Vec<Circuit>, connections: &[Connection]) {
+    (0..n).for_each(|i| {
+        let connection = &connections[i];
+        let a_idx = circuits
+            .iter()
+            .position(|c| c.has_box(connection.from_idx))
+            .unwrap();
+        let b_idx = circuits
+            .iter()
+            .position(|c| c.has_box(connection.to_idx))
+            .unwrap();
+
+        if a_idx == b_idx {
+            return; // Circuits are the same. Don't do anything
+        }
+
+        // Different circuits. Merge B into A
+        let b = circuits.swap_remove(b_idx);
+        circuits[a_idx].merge(&b);
+    });
+}
 
 fn build_connections(boxes: &[Box]) -> Vec<Connection> {
     let mut connections: Vec<Connection> = Vec::new();
@@ -36,7 +78,11 @@ fn build_connections(boxes: &[Box]) -> Vec<Connection> {
             let a = boxes[i];
             let b = boxes[j];
             let distance = get_distance(&a, &b);
-            connections.push(Connection { a, b, distance });
+            connections.push(Connection {
+                from_idx: i,
+                to_idx: j,
+                distance,
+            });
         }
     }
 
@@ -44,8 +90,21 @@ fn build_connections(boxes: &[Box]) -> Vec<Connection> {
     connections
 }
 
+fn init_circuits(count: usize) -> Vec<Circuit> {
+    (0..count).map(|i| Circuit { box_ids: vec![i] }).collect()
+}
+
 fn get_distance(a: &Box, b: &Box) -> f64 {
     ((b.x - a.x).powi(2) + (b.y - a.y).powi(2) + (b.z - a.z).powi(2)).sqrt()
+}
+
+// Scoring ====================================================================
+fn get_score(circuits: &[Circuit]) -> u64 {
+    if !circuits.is_sorted_by(|a, b| a.box_ids.len() >= b.box_ids.len()) {
+        panic!("Not sorted")
+    }
+    let lengths: Vec<u64> = circuits.iter().map(|c| c.box_ids.len() as u64).collect();
+    lengths[0] * lengths[1] * lengths[2]
 }
 
 // Parsing ====================================================================
@@ -96,22 +155,46 @@ mod test {
         let boxes = parse_input(&input);
         let connections = build_connections(&boxes);
 
-        assert_eq!(connections[0].a, get_box(162, 817, 812));
-        assert_eq!(connections[0].b, get_box(425, 690, 689));
+        assert_eq!(boxes[connections[0].from_idx], get_box(162, 817, 812));
+        assert_eq!(boxes[connections[0].to_idx], get_box(425, 690, 689));
 
-        assert_eq!(connections[1].a, get_box(162, 817, 812));
-        assert_eq!(connections[1].b, get_box(431, 825, 988));
+        assert_eq!(boxes[connections[1].from_idx], get_box(162, 817, 812));
+        assert_eq!(boxes[connections[1].to_idx], get_box(431, 825, 988));
     }
 
-    // #[test]
-    // fn part1_test_input() {
-    //     assert_eq!(part1("src/inputs/day08/test-input.txt", 10), 40);
-    // }
+    #[test]
+    fn test_merge_circuits() {
+        let mut circuit1 = Circuit { box_ids: vec![0] };
+        let mut circuit2 = Circuit { box_ids: vec![1] };
 
-    // #[test]
-    // fn part1_real() {
-    //     assert_eq!(part1("src/inputs/day08/input.txt", 1000), 5361735137219);
-    // }
+        circuit1.merge(&mut circuit2);
+        assert_eq!(circuit1.box_ids, vec![0, 1])
+    }
+
+    #[test]
+    fn test_join_closest_circuits() {
+        let input =
+            fs::read_to_string("src/inputs/day08/test-input.txt").expect("Couln't read the file");
+        let boxes = parse_input(&input);
+        let connections = build_connections(&boxes);
+        let mut circuits = init_circuits(boxes.len());
+        join_closest_circuits(10, &mut circuits, &connections);
+
+        circuits.sort_by(|a, b| b.box_ids.len().cmp(&a.box_ids.len()));
+        assert_eq!(circuits[0].box_ids.len(), 5);
+        assert_eq!(circuits[1].box_ids.len(), 4);
+        assert_eq!(circuits[2].box_ids.len(), 2);
+    }
+
+    #[test]
+    fn part1_test_input() {
+        assert_eq!(part1("src/inputs/day08/test-input.txt", 10), 40);
+    }
+
+    #[test]
+    fn part1_real() {
+        assert_eq!(part1("src/inputs/day08/input.txt", 1000), 115885);
+    }
 
     // #[test]
     // fn part2_test_input() {
